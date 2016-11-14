32442aadf6c5f171828d3b8b536735bcb4399457
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2016-11-14 15:37:41 +0100

    Change SimSettings from interface to class

--- src/main/java/com/okayboom/particlesim/SimSettings.java
+++ src/main/java/com/okayboom/particlesim/SimSettings.java
@@ -1,12 +1,50 @@
 package com.okayboom.particlesim;
 
-public interface SimSettings {
+public final class SimSettings {
 
-	double maxInitialVelocity();
+	public static SimSettings EMPTY = new SimSettings("", 0, 0, 0, 0);
 
-	int particleCount();
+	public static SimSettings DEFAULT = EMPTY.settingsName("Default settings")
+			.maxInitialVelocity(24).particleCount(10000).boxWidth(10000)
+			.boxHeight(10000);
 
-	long boxWidth();
+	public final String settingsName;
+	public final double maxInitialVelocity;
+	public final int particleCount;
+	public final int boxWidth;
+	public final int boxHeight;
 
-	long boxHeight();
+	SimSettings(String settingsName, double maxInitialVelocity,
+			int particleCount, int boxWidth, int boxHeight) {
+		this.settingsName = settingsName;
+		this.maxInitialVelocity = maxInitialVelocity;
+		this.particleCount = particleCount;
+		this.boxWidth = boxWidth;
+		this.boxHeight = boxHeight;
+	}
+
+	public SimSettings settingsName(String sn) {
+		return new SimSettings(sn, maxInitialVelocity, particleCount, boxWidth,
+				boxHeight);
+	}
+
+	public SimSettings maxInitialVelocity(double miv) {
+		return new SimSettings(settingsName, miv, particleCount, boxWidth,
+				boxHeight);
+	}
+
+	public SimSettings particleCount(int pc) {
+		return new SimSettings(settingsName, maxInitialVelocity, pc, boxWidth,
+				boxHeight);
+	}
+
+	public SimSettings boxWidth(int bw) {
+		return new SimSettings(settingsName, maxInitialVelocity, particleCount,
+				bw, boxHeight);
+	}
+
+	public SimSettings boxHeight(int bh) {
+		return new SimSettings(settingsName, maxInitialVelocity, particleCount,
+				boxWidth, bh);
+	}
 }
