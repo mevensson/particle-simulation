35ff1c485db744b13c94b6b79a9b6e16a6448d7c
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2016-11-14 15:39:03 +0100

    Change SimResult from interface to class

--- src/main/java/com/okayboom/particlesim/SimResult.java
+++ src/main/java/com/okayboom/particlesim/SimResult.java
@@ -1,10 +1,29 @@
 package com.okayboom.particlesim;
 
-public interface SimResult {
+public final class SimResult {
 
-	SimSettings givenSettings();
+	public static final SimResult EMPTY = new SimResult(null, 0, 0);
 
-	long totalStepCount();
+	public final double totalBoxMomentum;
+	public final long totalStepCount;
+	public final SimSettings simSettings;
 
-	double totalBoxMomentum();
+	SimResult(SimSettings simSettings, long totalStepCount,
+			double totalBoxMomentum) {
+		this.simSettings = simSettings;
+		this.totalStepCount = totalStepCount;
+		this.totalBoxMomentum = totalBoxMomentum;
+	}
+
+	public SimResult givenSettings(SimSettings ss) {
+		return new SimResult(ss, totalStepCount, totalBoxMomentum);
+	}
+
+	public SimResult totalStepCount(long tsc) {
+		return new SimResult(simSettings, tsc, totalBoxMomentum);
+	}
+
+	public SimResult totalBoxMomentum(double tbm) {
+		return new SimResult(simSettings, totalStepCount, tbm);
+	}
 }
