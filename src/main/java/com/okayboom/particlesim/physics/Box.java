4716690fa933978ceb33eaad8b87f14c6b7c7d5b
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2016-11-18 19:46:30 +0100

    Add Box intersection

--- src/main/java/com/okayboom/particlesim/physics/Box.java
+++ src/main/java/com/okayboom/particlesim/physics/Box.java
@@ -1,5 +1,7 @@
 package com.okayboom.particlesim.physics;
 
+import java.util.Optional;
+
 public final class Box {
 	public final Vector min;
 	public final Vector max;
@@ -12,4 +14,53 @@ public final class Box {
 	public static final Box box(double x1, double y1, double x2, double y2) {
 		return new Box(Vector.v(x1, y1), Vector.v(x2, y2));
 	}
+
+	public static final Box box(Vector a, Vector b) {
+		return new Box(a, b);
+	}
+
+	public Optional<Box> intersect(Box b2) {
+
+		Vector isectMin = min.max(b2.min);
+		Vector isectMax = max.min(b2.max);
+
+		boolean hasNoIntesection = isectMin.x >= isectMax.x
+				|| isectMin.y >= isectMax.y;
+
+		return hasNoIntesection ? Optional.empty() : Optional.of(box(isectMin,
+				isectMax));
+	}
+
+	@Override
+	public String toString() {
+		return "Box(min" + min + ", max" + max + ")";
+	}
+
+	@Override
+	public int hashCode() {
+		int result = ((max == null) ? 0 : max.hashCode());
+		return 31 * result + ((min == null) ? 0 : min.hashCode());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Box other = (Box) obj;
+		if (max == null) {
+			if (other.max != null)
+				return false;
+		} else if (!max.equals(other.max))
+			return false;
+		if (min == null) {
+			if (other.min != null)
+				return false;
+		}
+		return min.equals(other.min);
+	}
+
 }
--- src/test/java/com/okayboom/particlesim/physics/BoxTest.java
+++ src/test/java/com/okayboom/particlesim/physics/BoxTest.java
@@ -3,6 +3,8 @@ package com.okayboom.particlesim.physics;
 import static com.okayboom.particlesim.physics.Box.box;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Optional;
+
 import org.junit.Test;
 
 public class BoxTest {
@@ -13,5 +15,25 @@ public class BoxTest {
 
 		assertEquals(Vector.v(-1.5, 0), b.min);
 		assertEquals(Vector.v(-1, 4), b.max);
+	}
+
+	@Test
+	public void testNoIntersect() throws Exception {
+		Box b1 = box(0, 0, 1, 1);
+		Box b2 = box(0, 0, -1, -1);
+		Optional<Box> expected = Optional.empty();
+
+		assertEquals(expected, b1.intersect(b2));
+		assertEquals(expected, b2.intersect(b1));
+	}
+
+	@Test
+	public void testIntersect() throws Exception {
+		Box b1 = box(0, 0, 10, 1);
+		Box b2 = box(5, 0.5, 20, 20);
+		Optional<Box> expected = Optional.of(box(5, 0.5, 10, 1));
+
+		assertEquals(expected, b1.intersect(b2));
+		assertEquals(expected, b2.intersect(b1));
 	}
 }
