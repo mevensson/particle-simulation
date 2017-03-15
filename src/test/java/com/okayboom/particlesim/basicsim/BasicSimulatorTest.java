56107be035e4c26fec2acc0c696744c3a9f863e8
Mattias Evensson <mattias@evensson.eu>
2017-03-15 18:58:32 +0100

    Add test for BasicSimulator

---
+++ src/test/java/com/okayboom/particlesim/basicsim/BasicSimulatorTest.java
@@ -0,0 +1,35 @@
+package com.okayboom.particlesim.basicsim;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.okayboom.particlesim.SimResult;
+import com.okayboom.particlesim.SimSettings;
+
+public class BasicSimulatorTest {
+
+	@Test
+	public void shouldCalculateTotalBoxMomentumFast() {
+		final SimSettings settings = SimSettings.EMPTY.settingsName("Baseline")
+				.maxInitialVelocity(24).particleCount(2000).boxWidth(1000)
+				.boxHeight(1000).steps(10);
+		final BasicSimulator simulator = new BasicSimulator();
+		final SimResult result = simulator.simulate(settings);
+		assertThat(result.totalBoxMomentum, is(8387.0));
+	}
+
+	@Ignore
+	@Test
+	public void shouldCalculateTotalBoxMomentumSlow() {
+		final SimSettings settings = SimSettings.EMPTY.settingsName("Baseline")
+				.maxInitialVelocity(24).particleCount(2000).boxWidth(1000)
+				.boxHeight(1000).steps(500);
+		final BasicSimulator simulator = new BasicSimulator();
+		final SimResult result = simulator.simulate(settings);
+		assertThat(result.totalBoxMomentum, is(414892.0));
+	}
+
+}
