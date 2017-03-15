d191b00464fe87f2b285408a0b337c76fe391f95
Mattias Evensson <mattias@evensson.eu>
2017-03-15 20:17:03 +0100

    Rename and parameterize BasicSimulatorTest
    Also run it for QuadTreeSimulator.

--- src/test/java/com/okayboom/particlesim/basicsim/SimulatorTest.java
+++ src/test/java/com/okayboom/particlesim/basicsim/SimulatorTest.java
@@ -5,18 +5,36 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.okayboom.particlesim.SimResult;
 import com.okayboom.particlesim.SimSettings;
+import com.okayboom.particlesim.Simulator;
 
-public class BasicSimulatorTest {
+@RunWith(Parameterized.class)
+public class SimulatorTest {
+
+	@Parameters
+	public static Object[] data() {
+		return new Object[] {
+				new BasicSimulator(),
+				new QuadTreeSimulator()
+		};
+	}
+
+	private final Simulator simulator;
+
+	public SimulatorTest(final Simulator simulator) {
+		this.simulator = simulator;
+	}
 
 	@Test
 	public void shouldCalculateTotalBoxMomentumFast() {
 		final SimSettings settings = SimSettings.EMPTY.settingsName("Baseline")
 				.maxInitialVelocity(24).particleCount(2000).boxWidth(1000)
 				.boxHeight(1000).steps(10);
-		final BasicSimulator simulator = new BasicSimulator();
 		final SimResult result = simulator.simulate(settings);
 		assertThat(result.totalBoxMomentum, is(7695.0));
 	}
@@ -27,7 +45,6 @@ public class BasicSimulatorTest {
 		final SimSettings settings = SimSettings.EMPTY.settingsName("Baseline")
 				.maxInitialVelocity(24).particleCount(2000).boxWidth(1000)
 				.boxHeight(1000).steps(500);
-		final BasicSimulator simulator = new BasicSimulator();
 		final SimResult result = simulator.simulate(settings);
 		assertThat(result.totalBoxMomentum, is(381305.0));
 	}
